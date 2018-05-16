40101ffa307bc8f6d75d292d6c1c09596ade6d8c
Denis <hadaron@outlook.com>
2018-05-16 19:43:12 +0300

    Create checks table.

---
+++ database/migrations/2018_05_16_163401_create_checks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shop_id');
+            $table->timestampTz('time');
+            $table->timestamps();
+        });
+
+
+        Schema::table('checks', function ($table) {
+            $table->foreign('shop_id')->references('id')->on('shops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checks');
+    }
+}
